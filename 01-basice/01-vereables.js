21b58b21cea25055608a8ae399ef154271e63de4
Shubham17-Dev <santoshkumarshubham810@gmail.com>
2025-02-12 08:29:06 +0000

    ve

---
+++ 01-basice/01-vereables.js
@@ -0,0 +1,16 @@
+/*
+4 type of variables in js 
+1.const:-this variables can not change.
+2.let:-veriables change easyle
+3.var
+4automaticly
+*/
+
+
+
+
+const accountid = 12345
+let accountname="shubham"
+var amount="1235456"
+accountcity="delhi"
+console.table([accountid,accountname,amount,accountcity])
