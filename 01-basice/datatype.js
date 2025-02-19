9a09d39fe95491117a475f637d3fed866655534b
Shubham17-Dev <santoshkumarshubham810@gmail.com>
2025-02-19 07:12:59 +0000

    data

---
+++ 01-basice/datatype.js
@@ -0,0 +1,25 @@
+// let name = "shubham"
+// let age = 20
+// let logdin =true
+// console.table([name,age,logdin])
+// console.log(typeof name);
+
+
+// let age = "20mnm"
+// console.log(typeof age);
+
+// let numberage = Number(age)
+// console.log(typeof numberage);
+// console.log(numberage);
+
+let logdin ="cfvghbjkl"
+// console.log(typeof logdin);
+let booliyon = Boolean(logdin);
+console.log(booliyon)
+
+
+let num = 52
+// console.log(typeof num);
+let str = String(num)
+console.log(typeof str);
+console.log(str)
